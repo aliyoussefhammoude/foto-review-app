3d100fa4fd705ec87685184fb4e975c3173b406c
aliyoussefhammoude <ali.hammoude@hotmail.se>
2021-01-07 21:43:25 +0100

    cleaned up a little and put in navbar and the nav-dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { logRoles } from "@testing-library/react";
+import React from "react";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import logo from "./assets/images/logos.svg";
+import "./assets/scss/app.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return;
+    <>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/">
+                    <img
+                        alt="A photo album"
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{" "}
+                    Foto Review
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/albums">Albums</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Username" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/my-profile">My profile</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        <Container>Here be content.</Container>
+    </>;
+};
 
 export default App;
